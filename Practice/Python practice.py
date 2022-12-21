3efd807964aad08792e00c078f85d0efeb096a1d
sh9709 <kidwars82@gmail.com>
2022-12-21 11:16:29 +0900

    12-21 python update

---
+++ Practice/Python practice.py
@@ -0,0 +1,12 @@
+# 1. 아래 문자열의 길이를 구하시오.
+# q1 = "fjdldionelamdmsd'1322qdmfgvfvc'xv-=dfdfdsf"
+# print("1번 :",len("fjdldionelamdmsd'1322qdmfgvfvc'xv-=dfdfdsf"))
+
+# 2. 화면에 * 기호 100개를 표시하시오
+# for i in range(100):
+#     print("*")
+
+# 3. 문자열 "30"을 각각 정수형, 실수형, 복소수형, 문자형으로 변환하시오.
+word = "30"
+print("정수형 :",int(word),"실수형 :",float(word),"복소수형 :",complex(word),"문자형 :",chr(word))
+
