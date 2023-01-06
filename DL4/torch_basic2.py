9af899dd7f08b012ba5416001316f7a7bda755e6
sh9709 <kidwars82@gmail.com>
2023-01-06 15:06:46 +0900

    01-06 python update

---
+++ DL4/torch_basic2.py
@@ -0,0 +1,66 @@
+
+import torch
+
+print(torch.__version__)
+
+a = torch.tensor([5])
+b = torch.tensor([7])
+
+# Numpy 배열에서 텐서를 초기화
+c = (a + b).numpy()
+print(c)
+print(type(c))
+
+print("---------------------------")
+result = c * 10
+tensor = torch.from_numpy(result)
+print(tensor)
+print(type(tensor))
+
+print("---------------------------")
+# 다른 텐서의 정보를 토대로 텐서를 초기화할수 있음
+x = torch.tensor([
+    [5, 7],
+    [1, 2]
+])
+
+# x 와 같은 모양과 자료형을 가지지만, 값이 1인 텐서 생성
+x_ones = torch.ones_like(x)
+print(x_ones)
+print("---------------------------")
+
+# x 와 같은 모양을 가지되, 자료형은 float으로 덮어쓰고, 값은 랜덤으로 채우기
+x_rand = torch.rand_like(x, dtype=torch.float32)
+print(x_rand)
+print("---------------------------")
+
+# 텐서의 특정 차원 접근
+tensor = torch.tensor([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+])
+print(tensor.shape)
+print(tensor[0])                # first row
+print(tensor[:, 0])             # first column
+print(tensor[..., -1])          # last column
+
+print("---------------------------")
+
+# 두 텐서를 이어 붙여 연결하여 새로운 텐서를 생성
+
+# dim : 텐서를 이어 붙이기 위한 축
+# 0번 축(행)
+result = torch.cat([tensor, tensor, tensor], dim=0)
+print(result)
+
+# 1번 축(열)을 기준으로 이어 붙이기
+result = torch.cat([tensor, tensor, tensor], dim=1)
+print(result)
+
+print("---------------------------")
+
+# 텐서의 자료형 (정수, 실수 등)을 변환
+a = torch.tensor([2], dtype=torch.float32)
+print(a)
+print(a.dtype)
